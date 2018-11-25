e34a7eed4bdd148cfb21e7fd8fdc726bc929f856
瀬戸山智 <tomo@setoyamatomonoMacBook-puro.local>
2018-11-26 02:42:50 +0900

    Enabled scheduling

--- src/main/java/com/regsync/sample/ParseJson.java
+++ src/main/java/com/regsync/sample/ParseJson.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.util.ArrayList;
 
 import org.apache.http.HttpEntity;
@@ -13,13 +12,15 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SuppressWarnings("deprecation")
+@EnableScheduling
 public class ParseJson {
+	@Scheduled(cron = "0 0 * * * *", zone = "Asia/Tokyo")
 	public static void main(String[] args) {
 		StringBuilder builder = new StringBuilder();
 		// HttpClientのインスタンスを作る（HTTPリクエストを送るために必要）
