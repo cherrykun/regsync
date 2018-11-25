8154ad7ae2f6daf4fa79ae6a6435963c00388d8e
瀬戸山智 <tomo@setoyamatomonoMacBook-puro.local>
2018-11-26 02:11:17 +0900

    Initial commit

---
+++ src/main/java/com/regsync/sample/Main.java
@@ -0,0 +1,65 @@
+package com.regsync.sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void storingDb(String[] args) throws Exception {
+    	Connection conn = null;
+        Statement stmt = null;
+        ResultSet rset = null;
+
+        //接続文字列
+        String url = "jdbc:postgresql:postgres";
+        String user = "tomo";
+        String password = "";
+
+        try{
+            Class.forName("org.postgresql.Driver");
+
+            //PostgreSQLへ接続
+            conn = DriverManager.getConnection(url, user, password);
+
+            //自動コミットOFF
+            conn.setAutoCommit(false);
+
+            //SELECT文の実行
+            stmt = conn.createStatement();
+            String sql = "SELECT 1";
+            rset = stmt.executeQuery(sql);
+
+            //SELECT結果の受け取り
+            while(rset.next()){
+                String col = rset.getString(1);
+                System.out.println(col);
+            }
+
+            //INSERT文の実行
+            sql = "INSERT INTO jdbc_test VALUES (1, 'AAA')";
+            stmt.executeUpdate(sql);
+            conn.commit();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+        	//接続の切断
+            try {
+                if(rset != null)rset.close();
+                if(stmt != null)stmt.close();
+                if(conn != null)conn.close();
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
---
+++ src/main/java/com/regsync/sample/ParseJson.java
@@ -0,0 +1,87 @@
+package com.regsync.sample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SuppressWarnings("deprecation")
+public class ParseJson {
+	public static void main(String[] args) {
+		StringBuilder builder = new StringBuilder();
+		// HttpClientのインスタンスを作る（HTTPリクエストを送るために必要）
+		HttpClient client = new DefaultHttpClient();
+		ArrayList<Object> list = new ArrayList<Object>(); 
+		
+		for(int i=1; i<=10; i++) {
+		// HttpGetのインスタンスを作る（GETリクエストを送るために必要）
+		HttpGet httpGet = new HttpGet("http://fg-69c8cbcd.herokuapp.com/user/" + i);
+		try {
+			// リクエストしたリンクが存在するか確認するために、HTTPリクエストを送ってHTTPレスポンスを取得する
+			HttpResponse response = client.execute(httpGet);
+			// 返却されたHTTPレスポンスの中のステータスコードを調べる
+			// -> statusCodeが200だったらページが存在。404だったらNot found（ページが存在しない）。500はInternal server
+			// error。
+			int statusCode = response.getStatusLine().getStatusCode();
+			if (statusCode == 200) {
+				// HTTPレスポンスが200よりページは存在する
+				// レスポンスからHTTPエンティティ（実体）を生成
+				HttpEntity entity = response.getEntity();
+				// HTTPエンティティからコンテント（中身）を生成
+				InputStream content = entity.getContent();
+				// コンテントからInputStreamReaderを生成し、さらにBufferedReaderを作る
+				// InputStreamReaderはテキストファイル（InputStream）を読み込む
+				// BufferedReaderはテキストファイルを一行ずつ読み込む
+				// （参考）http://www.tohoho-web.com/java/file.htm
+				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+				String line;
+				// readerからreadline()で行を読んで、builder文字列(StringBuilderクラス)に格納していく。
+				// ※このプログラムの場合、lineは一行でなのでループは回っていない
+				// ※BufferedReaderを使うときは一般にこのように記述する。
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+					System.out.println("builder.append(line):" + builder.append(line));
+					System.out.println("line:" + line);
+					//JSONからJavaオブジェクトに変換
+					Object obj = getObjFromJSON(line);
+					System.out.println(obj);
+					//リストに格納
+					list.add(obj);
+				}
+			} else {
+				System.out.println("Failed to download file");
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		}
+		System.out.println(list);
+	}
+
+	//JSONをJavaオブジェクトに変換
+	private static Object getObjFromJSON(String line) {
+		ObjectMapper mapper = new ObjectMapper(); 
+		Object obj = null;
+	        try {
+	            obj = mapper.readValue(line, Object.class);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        return obj;
+	}
+}
